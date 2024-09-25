45c234da08a0e1b63909a2041068768d52adb5c3
msimonz <simon.marquezok@gmail.com>
2024-09-25 00:54:08 +0000

    correciones

--- pryintro-distri/src/servidor/InterfazServidor.java
+++ pryintro-distri/src/servidor/InterfazServidor.java
@@ -2,6 +2,6 @@ package servidor;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface InterfazServidor{
+public interface InterfazServidor extends Remote{
     public void hola() throws RemoteException;
 }
