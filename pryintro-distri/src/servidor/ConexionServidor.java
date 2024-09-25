284a6e6bae983e8026a2ae701905cc3d138c2931
msimonz <simon.marquezok@gmail.com>
2024-09-25 03:34:28 +0000

    creacion matriz

--- pryintro-distri/src/servidor/ConexionServidor.java
+++ pryintro-distri/src/servidor/ConexionServidor.java
@@ -1,15 +1,20 @@
 package servidor;
 import java.rmi.registry.Registry;
-
+import java.util.ArrayList;
 public class ConexionServidor{
     public static void main(String[] args){
         try{
             Registry r = java.rmi.registry.LocateRegistry.createRegistry(1099);
             r.rebind("InterfazServidor", new ImplementacionServidor());
-            System.out.println("Servidor creado con éxito");            
+            System.out.println("Servidor creado con éxito");
+            creacionMatriz();
+
         }catch(Exception e){
             System.out.println("Error en la creación del servidor: "+e); 
         }
-
+        
+    }
+    public void creacionMatriz(){
+        ArrayList<ArrayList<String>> matriz = new ArrayList<ArrayList<String>>();
     }
 }
