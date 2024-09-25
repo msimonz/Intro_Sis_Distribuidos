fe4bf17be1227c36383be9ae74577b14c70f4279
msimonz <simon.marquezok@gmail.com>
2024-09-25 03:55:09 +0000

    eliminacion funcion creacionMatriz

--- pryintro-distri/src/servidor/InterfazServidor.java
+++ pryintro-distri/src/servidor/InterfazServidor.java
@@ -3,7 +3,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface InterfazServidor extends Remote{
-    public void creacionMatriz() throws RemoteException;
     public int filasMatriz() throws RemoteException;
     public int columnasMatriz() throws RemoteException;
 }
