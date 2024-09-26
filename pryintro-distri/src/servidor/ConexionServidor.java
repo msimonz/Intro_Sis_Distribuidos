94a05462b5039746dc33a39822b3d8cddd18c36f
msimonz <simon.marquezok@gmail.com>
2024-09-26 06:03:53 +0000

    devolucion cambios de prueba

--- pryintro-distri/src/servidor/ConexionServidor.java
+++ pryintro-distri/src/servidor/ConexionServidor.java
@@ -3,7 +3,7 @@ import java.rmi.registry.Registry;
 public class ConexionServidor{
     public static void main(String[] args){
         try{
-            System.setProperty("java.rmi.server.hostname", "192.168.0.47");
+            System.setProperty("java.rmi.server.hostname", "192.168.0.49");
             Registry r = java.rmi.registry.LocateRegistry.createRegistry(1099);
             r.rebind("InterfazServidor", new ImplementacionServidor());
             System.out.println("Servidor creado con éxito");
